ef8ff17031c6f681cab34ec1460bb71e9f99d689
Tajudeen khabab Akanbi <152615493+TajjDev@users.noreply.github.com>
2024-02-09 18:57:39 +0100

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,8 @@
+// Display welcome page
+document.body.innerHTML = "<h1>Welcome!</h1>";
+
+// Wait for 5 seconds
+setTimeout(function() {
+    // Display main webpage
+    document.body.innerHTML = "<h1>Main Webpage</h1>";
+}, 5000);
